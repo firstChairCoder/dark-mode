969f050c294d7a4d99b66e7c6dfca89fbcc886fc
firstChairCoder <md1qq7@gmail.com>
2023-09-19 20:13:25 +0100

    feat(*)..

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ThemeProvider from './src/context/theme-context';
+import NavigationContainer from './src/navigation/NavigationContainer';
+import RootNavigator from './src/navigation';
+import { StatusBar } from './src/components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <NavigationContainer>
+        <StatusBar />
+        <RootNavigator />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
